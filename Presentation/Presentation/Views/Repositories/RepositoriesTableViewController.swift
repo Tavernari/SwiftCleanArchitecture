72750ff79a15a60692ee2ad41ad4e3da3f1ea7bc
Victor C Tavernari <victortavernari@gmail.com>
2020-03-07 08:53:12 -0300

    improve code

--- Presentation/Presentation/Views/Repositories/RepositoriesTableViewController.swift
+++ Presentation/Presentation/Views/Repositories/RepositoriesTableViewController.swift
@@ -9,8 +9,6 @@
 import UIKit
 import RxCocoa
 import RxSwift
-import Alamofire
-import AlamofireImage
 import Domain
 
 class RepositoriesTableViewController: UIViewController {
@@ -32,6 +30,27 @@ class RepositoriesTableViewController: UIViewController {
         tableView.estimatedRowHeight = 90
     }
 
+    fileprivate func populateCell(index: Int, repository: Repository, cell: RepositoriesTableViewCell) {
+        cell.repositoryAuthor = repository.author
+        cell.repositoryName = repository.name
+        cell.repositoryDescription = repository.description
+        cell.repositoryImage = repository.image
+        cell.repositoryForkCount = "\(repository.forkCount)"
+        cell.repositoryStarCount = "\(repository.starCount)"
+        cell.repositoryIssueCount = "\(repository.issuesCount)"
+    }
+
+    fileprivate func handleStatus(status: GitRepositoriesListStatus) {
+        switch status {
+        case .loading:
+            self.showLoadingIndicator(text: "Loading")
+        case .loaded:
+            self.removeLoadingIndicator()
+        case .fail(let errorMessage):
+            print(errorMessage)
+        }
+    }
+
     fileprivate func bindViewModel() {
         let cellIdentifier = R.reuseIdentifier.repositoriesTableViewCell.identifier
         viewModel
@@ -44,14 +63,7 @@ class RepositoriesTableViewController: UIViewController {
                     guard let cell = cell as? RepositoriesTableViewCell else {
                         return
                     }
-
-                    cell.repositoryAuthor = repository.author
-                    cell.repositoryName = repository.name
-                    cell.repositoryDescription = repository.description
-                    cell.repositoryImage = repository.image
-                    cell.repositoryForkCount = "\(repository.forkCount)"
-                    cell.repositoryStarCount = "\(repository.starCount)"
-                    cell.repositoryIssueCount = "\(repository.issuesCount)"
+                    self.populateCell(index: index, repository: repository, cell: cell)
             }.disposed(by: disposeBag)
 
         tableView
@@ -59,21 +71,12 @@ class RepositoriesTableViewController: UIViewController {
             .itemSelected
             .do(onNext: { self.tableView.cellForRow(at: $0)?.setSelected(false, animated: true) })
             .map{ $0.row }
-            .subscribe( viewModel.select )
+            .subscribe(viewModel.select)
             .disposed(by: self.disposeBag)
 
         viewModel
             .status
-            .subscribe(onNext: { status in
-                switch status {
-                case .loading:
-                    self.showLoadingIndicator(text: "Loading")
-                case .loaded:
-                    self.removeLoadingIndicator()
-                case .fail(let errorMessage):
-                    print(errorMessage)
-                }
-            })
+            .subscribe(onNext: self.handleStatus)
             .disposed(by: self.disposeBag)
     }
 
@@ -85,10 +88,5 @@ class RepositoriesTableViewController: UIViewController {
         title = "Repositories"
         
         self.viewModel.search.onNext("Javascript")
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
     }
 }
