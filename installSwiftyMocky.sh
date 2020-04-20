c734110bd8eb5caaae3025e93f7ddbaa9aefdd21
Victor C Tavernari <victortavernari@gmail.com>
2020-04-20 19:31:59 -0300

    Improve CI to use fastlane to generate mock instead of sh script

--- installSwiftyMocky.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install mint
-mint install MakeAWishFoundation/SwiftyMocky
-swiftymocky generate
