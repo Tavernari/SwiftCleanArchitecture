212856f648c945ebfadd5f2421d725571925d152
Victor C Tavernari <victortavernari@gmail.com>
2020-05-13 18:06:07 -0300

    Improve unit test

--- PresentationLayer/PresentationLayerTests/RepositoriesTableViewModelTests.swift
+++ PresentationLayer/PresentationLayerTests/RepositoriesTableViewModelTests.swift
@@ -17,50 +17,7 @@ class RepositoriesTableViewModelTests: XCTestCase {
         super.setUp()
     }
 
-    func testListRepository() {
-        let expectationLoadingStatus = XCTestExpectation(description: "Wait for loading status")
-        let expectationRepositories = XCTestExpectation(description: "Wait for repositories results")
-        let useCase = FetchGitRepositoriesUseCaseProtocolMock()
-        useCase.perform(.execute(term: .any, perform: { _ in
-            useCase.delegateInterfaceAdapter?.doing()
-            useCase.delegateInterfaceAdapter?.done(data: [GitRepositoryModel(), GitRepositoryModel()])
-        }))
-
-        let analyticsInterface = GitRepositoriesListViewModelAnalyticsProtocolMock()
-        let viewModel = GitRepositoriesListViewModel(
-            fetchGitRepositoriesUseCase: useCase,
-            delegateAnalyticsInterface: analyticsInterface
-        )
-        useCase.given(.delegateInterfaceAdapter(getter: viewModel))
-
-        viewModel.repositories.observe { repositories in
-            guard repositories.isEmpty == false else {
-                return
-            }
-
-            XCTAssertEqual(viewModel.repositories.value.count, 2)
-            expectationRepositories.fulfill()
-        }
-
-        viewModel.isLoading.observe { isLoading in
-            if isLoading == true {
-                expectationLoadingStatus.fulfill()
-            }
-        }
-
-        viewModel.search(term: "Java")
-
-        analyticsInterface.verify(.screen())
-        analyticsInterface.verify(.itemSelected(name: .any), count: .never)
-        analyticsInterface.verify(.searched(term: .value("Java")))
-
-        wait(for: [expectationRepositories, expectationLoadingStatus], timeout: 1)
-    }
-
-    func testSelectRepository() {
-        let expectationLoadingStatus = XCTestExpectation(description: "Wait for loading status")
-        let expectationRepositories = XCTestExpectation(description: "Wait for repositories results")
-
+    private func createUseCase() -> FetchGitRepositoriesUseCaseProtocolMock {
         var data1 = GitRepositoryModel()
         data1.name = "repository1DataName"
         data1.author = "repository1DataAuthor"
@@ -69,54 +26,51 @@ class RepositoriesTableViewModelTests: XCTestCase {
         data2.name = "repository2DataName"
         data2.author = "repository2DataAuthor"
 
-        let analyticsInterfaceMock = GitRepositoriesListViewModelAnalyticsProtocolMock()
-
         let useCaseMock = FetchGitRepositoriesUseCaseProtocolMock()
         useCaseMock.perform(.execute(term: .any, perform: { _ in
             useCaseMock.delegateInterfaceAdapter?.doing()
             useCaseMock.delegateInterfaceAdapter?.done(data: [data1, data2])
         }))
 
+        return useCaseMock
+    }
+
+    func testListRepository() {
+        let useCase = createUseCase()
+
+        let analyticsInterface = GitRepositoriesListViewModelAnalyticsProtocolMock()
+        let viewModel = GitRepositoriesListViewModel(
+            fetchGitRepositoriesUseCase: useCase,
+            delegateAnalyticsInterface: analyticsInterface
+        )
+        useCase.given(.delegateInterfaceAdapter(getter: viewModel))
+
+        viewModel.search(term: "Java")
+
+        analyticsInterface.verify(.screen())
+        analyticsInterface.verify(.itemSelected(name: .any), count: .never)
+        analyticsInterface.verify(.searched(term: .value("Java")))
+        XCTAssertEqual(viewModel.repositories.value.count, 2)
+    }
+
+    func testSelectRepository() {
+        let analyticsInterfaceMock = GitRepositoriesListViewModelAnalyticsProtocolMock()
+        let useCaseMock = createUseCase()
         let viewModel = GitRepositoriesListViewModel(
             fetchGitRepositoriesUseCase: useCaseMock,
             delegateAnalyticsInterface: analyticsInterfaceMock
         )
         useCaseMock.given(.delegateInterfaceAdapter(getter: viewModel))
 
-        viewModel.repositories.observe { repositories in
-            guard repositories.isEmpty == false else {
-                return
-            }
-
-            XCTAssertEqual(viewModel.repositories.value.count, 2)
-            expectationRepositories.fulfill()
-        }
-
-        viewModel.isLoading.observe { isLoading in
-            if isLoading == true {
-                expectationLoadingStatus.fulfill()
-            }
-        }
-
-        viewModel.repositories.observe { repositories in
-            guard repositories.isEmpty == false else {
-                return
-            }
-
-            let index = 1
-            let gitRepo = repositories[index]
-            XCTAssertEqual(viewModel.repositories.value.count, 2)
-            expectationRepositories.fulfill()
-            viewModel.select(index: index)
-            XCTAssertEqual(viewModel.route.value, GitRepositoriesListViewModelRoute.showPullRequests(repo: gitRepo))
-        }
-
         viewModel.search(term: "Swift")
+        viewModel.select(index: 1)
 
         analyticsInterfaceMock.verify(.screen())
         analyticsInterfaceMock.verify(.searched(term: .value("Swift")))
-        analyticsInterfaceMock.verify(.itemSelected(name: .value(data2.name)))
+        analyticsInterfaceMock.verify(.itemSelected(name: .value("repository2DataName")))
 
-        wait(for: [expectationRepositories, expectationLoadingStatus], timeout: 1)
+        let gitRepoSelected = viewModel.repositories.value[1]
+        let route = GitRepositoriesListViewModelRoute.showPullRequests(repo: gitRepoSelected)
+        XCTAssertEqual(viewModel.route.value, route)
     }
 }
