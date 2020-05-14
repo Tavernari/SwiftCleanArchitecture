d88d3bbb60d3869f5c07c4a95042539ed80d26f4
Lucas Fernandes <lucas.fernandes.silveira@gmail.com>
2020-05-14 10:49:08 -0300

    Refactor Sign UseCases to use email and password extensions validators instead of specific funcs inside the UseCases

--- DomainLayer/DomainLayer/SignIn/RecoverPassword/RecoverPasswordUseCase.swift
+++ DomainLayer/DomainLayer/SignIn/RecoverPassword/RecoverPasswordUseCase.swift
@@ -28,15 +28,8 @@ public class RecoverPasswordUseCase {
         self.repository = repository
     }
 
-    private func isValidEmail(_ email: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-
-        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
-    }
-
     public func execute(email: String) {
-        guard isValidEmail(email) else {
+        guard email.isValidEmail() else {
             delegateInterfaceAdapter?.invalidEmail()
             return
         }
