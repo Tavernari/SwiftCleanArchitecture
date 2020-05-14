a7373fe9717e3c891e2c50f881e5a289c9266f02
Lucas Fernandes <lucas.fernandes.silveira@gmail.com>
2020-05-14 15:04:00 -0300

    Refactor LoginUseCaseTests to conform CodeClimate

--- DomainLayer/DomainLayerTests/UseCases/LoginUseCaseTests.swift
+++ DomainLayer/DomainLayerTests/UseCases/LoginUseCaseTests.swift
@@ -52,11 +52,11 @@ class LoginUseCaseTests: XCTestCase {
 
     override func setUp() {
         paramsState = LoginUseCaseParams(customToken: "token123",
-                                         startedAuth: true,
-                                         logedIn: true,
-                                         failureOnLogin: true,
-                                         invalidEmail: true,
-                                         invalidPassword: true)
+                                         startedAuth: false,
+                                         logedIn: false,
+                                         failureOnLogin: false,
+                                         invalidEmail: false,
+                                         invalidPassword: false)
     }
 
     func loadLoginUseCase(params: LoginUseCaseParams) -> LoginUseCase {
@@ -77,9 +77,8 @@ class LoginUseCaseTests: XCTestCase {
     }
 
     func testLoginWithValidEmailAndPassword_shouldReturnTrue() {
-        paramsState.failureOnLogin = false
-        paramsState.invalidEmail = false
-        paramsState.invalidPassword = false
+        paramsState.startedAuth = true
+        paramsState.logedIn = true
 
         let useCase = loadLoginUseCase(params: paramsState)
         useCase.execute(email: "lucas@email.com", password: "pass123")
@@ -87,27 +86,27 @@ class LoginUseCaseTests: XCTestCase {
 
     func testLoginWithValidEmailAndPassword_shouldReturnFalse() {
         paramsState.customToken = ""
-        paramsState.logedIn = false
-        paramsState.invalidEmail = false
-        paramsState.invalidPassword = false
+        paramsState.startedAuth = true
+        paramsState.logedIn = true
+        paramsState.failureOnLogin = true
 
         let useCase = loadLoginUseCase(params: paramsState)
         useCase.execute(email: "lucas@email.com", password: "pass123")
     }
 
     func testLoginWithInvalidEmailAndPassword_shouldReturnFalse() {
-        paramsState.logedIn = false
-        paramsState.failureOnLogin = false
-        paramsState.invalidPassword = false
+        paramsState.startedAuth = true
+        paramsState.logedIn = true
+        paramsState.invalidEmail = true
 
         let useCase = loadLoginUseCase(params: paramsState)
         useCase.execute(email: "lucas@email", password: "pass123")
     }
 
     func testLoginWithInvalidPassword_shouldReturnFalse() {
-        paramsState.logedIn = false
-        paramsState.failureOnLogin = false
-        paramsState.invalidEmail = false
+        paramsState.startedAuth = true
+        paramsState.logedIn = true
+        paramsState.invalidPassword = true
 
         let useCase = loadLoginUseCase(params: paramsState)
         useCase.execute(email: "lucas@email.com", password: "pass")
