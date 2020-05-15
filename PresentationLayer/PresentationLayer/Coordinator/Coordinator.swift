e1fc612abfbcfd7a5fc8e70101522edf651028b8
Victor C Tavernari <victortavernari@gmail.com>
2020-05-15 12:04:50 -0300

    Improve ci job

--- PresentationLayer/PresentationLayer/Coordinator/Coordinator.swift
+++ PresentationLayer/PresentationLayer/Coordinator/Coordinator.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol Coordinator: class {
+protocol Coordinator: AnyObject {
     var childrens: [Coordinator] { get set }
     init(withNavigation navigationController: UINavigationController, assembler: MainAssembler)
     func start()
